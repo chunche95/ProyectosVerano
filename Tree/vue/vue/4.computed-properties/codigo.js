3658a7d6b410ffbea062dc6a88612608561d816a
chunche95 <paeste95.pb@gmail.com>
2024-08-25 13:27:26 +0200

    Add. Computed properties

--- Tree/vue/vue/4.computed-properties/codigo.js
+++ Tree/vue/vue/4.computed-properties/codigo.js
@@ -24,6 +24,29 @@ const app = new Vue({
     },
     tareasCompletadas() {
       return this.tareas.filter(tarea => tarea.estado)
+    },
+    mostrarNombre() {
+      const isThere = 'navigator' in window;
+      let os = "";
+      const userAgent = isThere && 'userAgent' in navigator && navigator.userAgent.toLowerCase() || '';
+      const vendor = isThere && 'vendor' in navigator && navigator.vendor.toLowerCase() || '';
+      const appVersion = isThere && 'appVersion' in navigator && navigator.appVersion.toLowerCase() || '';
+      const platform = isThere && 'platform' in navigator && navigator.platform.toLowerCase() || '';
+
+      if (/iphone/i.test(userAgent) || /ipad/i.test(userAgent) || /ipod/i.test(userAgent)) os = 'ios';
+      if (/android/i.test(userAgent)) os = 'android'
+      if (/win/i.test(appVersion) && /phone/i.test(userAgent)) os = 'windowsPhone'
+      if (/mac/i.test(appVersion)) os = 'MacOSX'
+      if (/win/i.test(appVersion)) os = 'windows'
+      if (/linux/i.test(appVersion)) os = 'linux'
+
+      return {
+        os,
+        userAgent,
+        vendor,
+        appVersion,
+        platform,
+      }
     }
   }
 })
